ec3897fdaa8ec7fd9f500fc87c54bece5de82a1f
Талгат Ракишев <>
2021-03-17 23:42:34 +0600

    first commit

---
+++ js/users.js
@@ -0,0 +1,31 @@
+/*eslint-env es6*/
+
+const requestURL = 'http://localhost:8080/user/';
+
+function sendRequest(method, url, body = null) {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+
+        xhr.open(method, url);
+
+        xhr.responseType = 'json';
+
+        xhr.onload = () => {
+            if (xhr.status >= 400) {
+                console.error(xhr.response);
+            } else {
+                document.write(xhr.response);
+            }
+        }
+
+        xhr.onerror = () => {
+            document.write("ERROR: " + xhr.response)
+        }
+
+        xhr.send();
+    })
+}
+
+sendRequest('GET', requestURL)
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
