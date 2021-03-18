f7c94624a58f7cbd88d327d7727ff320277f898c
t1gerok.tr@gmail.com <muhan0va>
2021-03-18 20:47:06 +0600

    hours

---
+++ js/hours.js
@@ -0,0 +1,33 @@
+/*eslint-env es6*/
+
+const requestURL = 'http://localhost:8080/hours/';
+
+function sendRequest(method, url, body = null) {
+    return fetch(url).then(response => {
+        return response.json();
+    })
+}
+
+sendRequest('GET', requestURL)
+    .then(data => viewUsers(data))
+    .catch(err => console.log(err));
+
+// function viewUsers (array){
+//     $.each(array, function (index, i) {
+//         $('#htList').append("<li>" + " " + i.id + ", " + i.firstname + ", " + i.lastname + ", " + i.email + " " + "</li>");
+//     })
+// };
+
+function viewUsers(array) {
+    $.each(array, function (index, i) {
+        $('.users-row').append(
+            '<div class="col-4">' +
+                '<div class="user">' +
+                    '<div class="id">Ид: ' + i.id + '</div>' +
+                    '<div class="fname">Имя: ' + i.firstname + '</div>' +
+                    '<div class="lname">Фамилия: ' + i.lastname + '</div>' +
+                    '<div class="email">E-mail: ' + i.email + '</div>' +
+                '</div>' +
+            '</div>');
+    })
+};
