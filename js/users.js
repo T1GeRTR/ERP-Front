533bdaf581808c6906e442d25471c0a9c2d75408
t1gerok.tr@gmail.com <muhan0va>
2021-03-18 20:45:44 +0600

    hours

--- js/users.js
+++ js/users.js
@@ -2,8 +2,6 @@
 
 const requestURL = 'http://localhost:8080/user/';
 
-var getUsers;
-
 function sendRequest(method, url, body = null) {
     return fetch(url).then(response => {
         return response.json();
@@ -11,11 +9,25 @@ function sendRequest(method, url, body = null) {
 }
 
 sendRequest('GET', requestURL)
-    .then(data => getUsers = data)
+    .then(data => viewUsers(data))
     .catch(err => console.log(err));
 
-$(document).ready(function () {
-    $.each(getUsers, function (index, i) {
-        $('#htList').append("<li>*" + " TEST " + i.id + ", " + i.firstname + ", " + i.lastname + ", " + i.email + " " + "</li>");
+// function viewUsers (array){
+//     $.each(array, function (index, i) {
+//         $('#htList').append("<li>" + " " + i.id + ", " + i.firstname + ", " + i.lastname + ", " + i.email + " " + "</li>");
+//     })
+// };
+
+function viewUsers(array) {
+    $.each(array, function (index, i) {
+        $('.users-row').append(
+            '<div class="col-3">' +
+            '<div class="user">' +
+            '<div class="id">Ид: ' + i.id + '</div>' +
+            '<div class="fname">Имя: ' + i.firstname + '</div>' +
+            '<div class="lname">Фамилия: ' + i.lastname + '</div>' +
+            '<div class="email">E-mail: ' + i.email + '</div>' +
+            '</div>' +
+            '</div>');
     })
-}());
+};
